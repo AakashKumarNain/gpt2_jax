a53213367aa682c2877fe7cf2897953c3f0ebe2d
AakashKumarNain <aakashnain@outlook.com>
2024-08-19 16:28:35 +0530

    use helper methods from utils file

--- gpt2_jax/model.py
+++ gpt2_jax/model.py
@@ -10,24 +10,10 @@ from typing import NamedTuple
 import equinox as eqx
 from equinox._misc import default_floating_dtype
 
-
-def count_params(model):
-    return sum(x.size for x in jtu.tree_leaves(eqx.filter(model, eqx.is_array)))
-
-
-def get_weight_and_bias(module):
-    if hasattr(module, "bias") and module.bias is not None:
-        return module.weight, module.bias
-    return module.weight
-
-
-def set_weight_and_bias(weight, bias, key, mean=0.0, std=0.02):
-    init = jax.nn.initializers.normal(stddev=std)
-    weight = init(key=key, shape=weight.shape).astype(weight.dtype)
-    if bias is not None:
-        bias = jnp.zeros_like(bias, dtype=weight.dtype)
-        return weight, bias
-    return weight
+from utils import count_params
+from utils import get_weight_and_bias
+from utils import set_weight_and_bias
+from utils import scaled_dot_product_attention
 
 
 class MLP(eqx.Module):
