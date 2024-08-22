d99f70dc3ae0f4f933975b93007ac742014efc8b
AakashKumarNain <aakashnain@outlook.com>
2024-08-22 14:44:58 +0530

    add hparams

--- gpt2_jax/train.py
+++ gpt2_jax/train.py
@@ -48,6 +48,39 @@ def main(text_file_path):
     total_batch_size = 524288 # ideal batch size as in GPT2 paper
     num_devices = len(jax.device_count("gpu"))
     grad_accum_steps = total_batch_size // (batch_size * config.block_size * num_devices)
+
+    assert total_batch_size % (batch_size * config.block_size * num_devices) == 0, \
+        "make sure total_batch_size is divisible by B * T * num_devices"
+
+    max_lr = 6e-4
+    min_lr = max_lr * 0.1
+    warmup_steps = 715
+    max_steps = 19073
+    b1 = 0.9
+    b2 = 0.95
+    weight_decay = 0.1
+    grad_clip_norm = 1.0
+
+
+    print("\n", "="*30, "Model config values", "="*30, "\n")
+    print(f"Block size (Sequence length)           : {config.block_size}")
+    print(f"Vocabulary size                        : {config.vocab_size}")
+    print(f"Number of transformer layers           : {config.num_layers}")
+    print(f"Number of heads                        : {config.num_heads}")
+    print(f"Embedding dimension size               : {config.embed_dim}")
+
+    print("\n", "="*30, "Hyperparams values", "="*30, "\n")
+    print(f"Batch size as per paper                : {total_batch_size}")
+    print(f"Max batch size that can be fitted here : {batch_size}")
+    print(f"Number of gradient accumulation steps  : {grad_accum_steps}")
+    print(f"Number of training steps               : {total_train_steps}")
+    print(f"Weight decay                           : {weight_decay}")
+    print(f"Minimum learning rate                  : {min_lr:.6f}")
+    print(f"Maximum learning rate                  : {max_lr:.6f}")
+    print(f"Warmup steps                           : {warmup_steps}")
+    print(f"Decay steps                            : {total_train_steps - warmup_steps}")
+    print(f"Adam betas values                      : {betas}\n")
+    print(f"Using device                           : {device}")
     
     dl = SimpleDataLoader(
         batch_size=batch_size,
@@ -57,10 +90,6 @@ def main(text_file_path):
         split="train"
     )
 
-    max_lr = 6e-4
-    min_lr = max_lr * 0.1
-    warmup_steps = 715
-    max_steps = 19073
 
     # Filter out the parameters so that we apply weight decay to selected
     # parameters only
